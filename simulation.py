6982696d2906d005647f13c374cbe209db2f6375
Ricardo Quinteiro <34577049+RicardoQuinteiro@users.noreply.github.com>
2019-11-09 18:10:34 +0000

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -2,19 +2,22 @@ import pygame
 
 pygame.init()
 
+#Creating window and setting size
 win = pygame.display.set_mode((500,500))
+#Name of the app
 pygame.display.set_caption("Schelling Model Simulation")
 
 run = True
 while run:
 	pygame.time.delay(100)
 
+	#Check if exit button was pressed to exit the window
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
 
-	pygame.draw.circle(win, (0, 0, 255), (250, 250), 20)
-	pygame.display.update()
-	#O Pedro é burro
+	if run:
+		pygame.draw.circle(win, (0, 0, 255), (250, 250), 20)
+		pygame.display.update()
 
 pygame.quit()
