844114041796715728f2f53f0c68a6f2b202d225
Ricardo Quinteiro <34577049+RicardoQuinteiro@users.noreply.github.com>
2019-11-09 20:09:21 +0000

    overall update

--- simulation.py
+++ simulation.py
@@ -5,12 +5,12 @@ from text_box import text_box
 pygame.init()
 
 #Creating window and setting size
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((500,500), pygame.FULLSCREEN)
 #Name of the app
 pygame.display.set_caption("Schelling Model Simulation")
 
 text_boxes = []
-text_boxes += [text_box(40, 100, 100, 20, border=2)]
+text_boxes += [text_box(40, 100, 100, 20, border=2, is_number=True)]
 text_boxes += [text_box(40, 150, 100, 20, border=2)]
 text_boxes += [text_box(40, 200, 100, 20, border=2)]
 
@@ -28,9 +28,13 @@ while run:
 			for box in text_boxes:
 				box.check_click(pygame.mouse.get_pos())
 		if event.type == pygame.KEYDOWN:
-			for box in text_boxes:
-				if box.active:
-					box.add_text(event.key)
+			if event.key == 27:
+				pygame.quit()
+				sys.exit()
+			else:
+				for box in text_boxes:
+					if box.active:
+						box.add_text(event.key)
 
 
 	#Update
--- text_box.py
+++ text_box.py
@@ -3,7 +3,8 @@ vec = pygame.math.Vector2
 
 class text_box():
 
-	def __init__(self, x, y, width, height, bg_colour=(166,166,166), active_colour=(255,255,255), text_size=12, text_colour=(0,0,0), border=0, border_colour=(0,0,0)):
+	def __init__(self, x, y, width, height, bg_colour=(166,166,166), active_colour=(255,255,255), 
+		text_size=12, text_colour=(0,0,0), border=0, border_colour=(0,0,0), is_number=False):
 		
 		self.x = x
 		self.y = y
@@ -21,6 +22,9 @@ class text_box():
 		self.text_colour = text_colour
 		self.border = border
 		self.border_colour = border_colour
+		self._numbers = [48,49,50,51,52,53,54,55,56,57]
+		#self._special = [8,13,32]
+		self.is_number = is_number
 
 	def update(self):
 		pass
@@ -49,15 +53,60 @@ class text_box():
 			#Rendering text to image
 			text = self.font.render(self.text, False, self.text_colour)
 			text_height = text.get_height()
-			self.image.blit(text, (self.border*2, (self.height - text_height)//2))
+			text_width = text.get_width()
+			if text_width < self.width - self.border*2:
+				self.image.blit(text, (self.border*2, (self.height - text_height)//2))
+			else:
+				self.image.blit(text, ((self.border*2)+(self.width-text_width-self.border*3), (self.height - text_height)//2))
 
 		window.blit(self.image, self.pos)
 
 	def add_text(self, key):
-		text = list(self.text)
-		text.append(chr(key))
-		self.text = ''.join(text)
-		print(self.text)
+		try:
+			if not self.is_number:
+				#Adding letters
+				if chr(key).isalpha():
+					text = list(self.text)
+					text.append(chr(key))
+					self.text = ''.join(text)
+					print(self.text)
+				#Backspace
+				elif key == 8:
+					text = list(self.text)
+					text.pop()
+					self.text = ''.join(text)
+				#Space
+				elif key == 8:
+					text = list(self.text)
+					text.append(' ')
+					self.text = ''.join(text)
+				#Numbers
+				elif key in self._numbers:
+					text = list(self.text)
+					text.append(str(key-48))
+					self.text = ''.join(text)
+				#Dot
+				elif key == 46:
+					text = list(self.text)
+					text.append('.')
+					self.text = ''.join(text)
+				else:
+					print(key)
+			else:
+				#Numbers
+				if key in self._numbers:
+					text = list(self.text)
+					text.append(str(key-48))
+					self.text = ''.join(text)
+				#Dot
+				elif key == 46:
+					text = list(self.text)
+					text.append('.')
+					self.text = ''.join(text)
+				else:
+					print(key)
+		except:
+			print(key)
 
 	def check_click(self, pos):
 		if pos[0] > self.x and pos[0] < self.x + self.width:
@@ -67,3 +116,9 @@ class text_box():
 				self.active = False
 		else:
 			self.active = False
+
+	def return_value(self):
+		if not is_number:
+			return self.text
+		else:
+			return self.float(text)
